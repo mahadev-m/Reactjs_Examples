c6a4eb0490bb63a3e20b0c498330dd3cab493021
mahadev-m <76099581+mahadev-m@users.noreply.github.com>
2021-02-23 15:50:11 +0530

    Add files via upload

--- react_name/src/App.js
+++ react_name/src/App.js
@@ -1,1 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+import Product from "./product";
+import 
+const products = [
+  {
+    id: 1,
+    productName: "apple",
+  },
+];
 
+const App = () => {
+  const [value,setValue]=useState(false);
+  return (
+    <div className="App">
+      <button>show</button>
+      {
+        value ?  
+        {products.map((Product) => {
+        return <div key={products.id}>{Product.productName}</div>;
+      })}
+      }
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ react_name/src/product.js
@@ -0,0 +1,10 @@
+import App from "./App";
+
+const Product = () => {
+  return (
+    <div>
+      <h1>Welcome</h1>
+    </div>
+  );
+};
+export default Product;
